4158f56c7a71025bef1724a6e4e65d5583324bed
GunWoo <kkw31025@gmail.com>
2025-12-03 14:13:44 +0900

    feat: add redirect page

---
+++ src/components/Redirect.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+리다이렉트 페이지
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import Intro from '../components/Intro.vue'
 import Home from '../components/Home.vue'
 import keycloak from '../keycloak';
+import Redirect from '../components/Redirect.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'Intro', component: Intro },
@@ -11,6 +12,12 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: Home,
     meta: { requiresAuth: true },
+  },
+  {
+    path: '/Redirect',
+    name: 'Redirect',
+    component: Redirect,
+    meta: {requiresAuth: true},
   },
 ];
 
@@ -24,8 +31,8 @@ router.beforeEach(async (to, from, next) => {
   if (to.meta.requiresAuth) {
     if (!keycloak.authenticated) {
       // 로그인 강제
-      await keycloak.login({ redirectUri: window.location.origin + to.fullPath });
-      return; // 로그인으로 리다이렉트되므로 next() 호출 X
+      await keycloak.login({ redirectUri: window.location.origin + to.fullPath }); // 초대 링크로 접속 시 로그인 완료 후 원래 uri로 진입, 즉 초대 링크에 접속됨
+      return; 
     }
   }
   next();
